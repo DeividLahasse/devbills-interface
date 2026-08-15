0fd7e0fc01b5a951a530ef983f442feb37fa970d
Deivid Santos Lahasse <deividlahasse@gmail.com>
2026-08-15 15:36:49 -0300

    fix: corrigir endpoints de transações

--- src/services/transactionService.ts
+++ src/services/transactionService.ts
@@ -1,18 +1,32 @@
-import type { CreateTransactionDTO, MonthlyItem, TranasactionFilter, Transaction, TransactionSummary } from "../types/transactions";
+import type {
+  CreateTransactionDTO,
+  MonthlyItem,
+  TranasactionFilter,
+  Transaction,
+  TransactionSummary,
+} from "../types/transactions";
 import { api } from "./api";
 
-export const getTransactions = async (filter?: Partial<TranasactionFilter>): Promise<Transaction[]> => {
-  const reponse = await api.get<Transaction[]>("/transactions", {
+export const getTransactions = async (
+  filter?: Partial<TranasactionFilter>,
+): Promise<Transaction[]> => {
+  const response = await api.get<Transaction[]>("/api/transactions", {
     params: filter,
   });
 
-  return reponse.data;
+  return response.data;
 };
 
-export const getTransactionSummary = async (month: number, year: number): Promise<TransactionSummary> => {
-  const response = await api.get<TransactionSummary>("/transactions/summary", {
-    params: { month, year },
-  });
+export const getTransactionSummary = async (
+  month: number,
+  year: number,
+): Promise<TransactionSummary> => {
+  const response = await api.get<TransactionSummary>(
+    "/api/transactions/summary",
+    {
+      params: { month, year },
+    },
+  );
 
   return response.data;
 };
@@ -22,7 +36,7 @@ export const getTransactionsMonthly = async (
   year: number,
   months?: number,
 ): Promise<{ history: MonthlyItem[] }> => {
-  const response = await api.get("/transactions/historical", {
+  const response = await api.get("/api/transactions/historical", {
     params: {
       month,
       year,
@@ -33,12 +47,17 @@ export const getTransactionsMonthly = async (
   return response.data;
 };
 
-export const deleteTransactions = async(id: string):Promise<void> =>{
-await api.delete(`/transactions/${id}`)
-}
+export const deleteTransactions = async (id: string): Promise<void> => {
+  await api.delete(`/api/transactions/${id}`);
+};
 
-export const createTrasaction = async(transactionData:CreateTransactionDTO):Promise<Transaction> =>{
-    const response = await api.post<Transaction>("/transactions", transactionData)
+export const createTrasaction = async (
+  transactionData: CreateTransactionDTO,
+): Promise<Transaction> => {
+  const response = await api.post<Transaction>(
+    "/api/transactions",
+    transactionData,
+  );
 
-    return response.data;
-}
+  return response.data;
+};
